70a72ce621be017fd6c85c380eaa9cdced7527df
Yi-Ju Tseng <beergt@gmail.com>
2017-11-12 09:53:20 -0600

    Update HW4.java

--- HW4.java
+++ HW4.java
@@ -8,10 +8,10 @@ public class HW4 {
 		//TODO: please check your output, make sure that 
 		Player player1=new Player("Player 1",100);
 		Player player2=new Player("Player 2",300);
-		player1.say_hello();
-		player2.say_hello();
-		int p1Bet=player1.make_bet();
-		int p2Bet=player2.make_bet();
+		player1.sayHello();
+		player2.sayHello();
+		int p1Bet=player1.makeBet();
+		int p2Bet=player2.makeBet();
 		ArrayList<Card> player1Card=new ArrayList<Card>();
 		player1Card.add(deck.getOneCard(true));
 		player1Card.add(deck.getOneCard(true));
@@ -23,19 +23,19 @@ public class HW4 {
 		player2.setOneRoundCard(player2Card);
 		boolean hit=false;
 		do{
-			hit=player1.hit_me(); //this
+			hit=player1.hitMe(); //this
 			if(hit){
 				player1Card.add(deck.getOneCard(true));
 				player1.setOneRoundCard(player1Card);
 				System.out.print("Hit! ");
-				System.out.println(player1.get_name()+"'s Cards now:");
+				System.out.println(player1.getName()+"'s Cards now:");
 				for(Card c : player1Card){
 					c.printCard();
 				}
 			}
 			else{
-				System.out.println(player1.get_name()+", Pass hit!");
-				System.out.println(player1.get_name()+", Final Card:");
+				System.out.println(player1.getName()+", Pass hit!");
+				System.out.println(player1.getName()+", Final Card:");
 				for(Card c : player1Card){
 					c.printCard();
 				}
@@ -44,19 +44,19 @@ public class HW4 {
 		
 		hit=false;
 		do{
-			hit=player2.hit_me(); //this
+			hit=player2.hitMe(); //this
 			if(hit){
 				player2Card.add(deck.getOneCard(true));
 				player2.setOneRoundCard(player2Card);
 				System.out.print("Hit! ");
-				System.out.println(player2.get_name()+"'s Cards now:");
+				System.out.println(player2.getName()+"'s Cards now:");
 				for(Card c : player2Card){
 					c.printCard();
 				}
 			}
 			else{
-				System.out.println(player2.get_name()+", Pass hit!");
-				System.out.println(player2.get_name()+", Final Card:");
+				System.out.println(player2.getName()+", Pass hit!");
+				System.out.println(player2.getName()+", Final Card:");
 				for(Card c : player2Card){
 					c.printCard();
 				}
@@ -65,26 +65,26 @@ public class HW4 {
 		if(player1.getTotalValue()>21 && player2.getTotalValue()>21){
 			System.out.println("Need another game");
 		}else if(player1.getTotalValue()<=21&&player2.getTotalValue()>21){
-			System.out.println(player1.get_name()+" wins the game");
-			player1.increase_chips(p1Bet);
-			player2.increase_chips(-p2Bet);
+			System.out.println(player1.getName()+" wins the game");
+			player1.increaseChips(p1Bet);
+			player2.increaseChips(-p2Bet);
 		}else if(player1.getTotalValue()>21&&player2.getTotalValue()<=21){
-			System.out.println(player2.get_name()+" wins the game");
-			player1.increase_chips(-p1Bet);
-			player2.increase_chips(p2Bet);
+			System.out.println(player2.getName()+" wins the game");
+			player1.increaseChips(-p1Bet);
+			player2.increaseChips(p2Bet);
 		}else if(player1.getTotalValue()>player2.getTotalValue()&&player1.getTotalValue()<=21){
-			System.out.println(player1.get_name()+" wins the game");
-			player1.increase_chips(p1Bet);
-			player2.increase_chips(-p2Bet);
+			System.out.println(player1.getName()+" wins the game");
+			player1.increaseChips(p1Bet);
+			player2.increaseChips(-p2Bet);
 		}else if(player1.getTotalValue()<player2.getTotalValue()&&player2.getTotalValue()<=21){
-			System.out.println(player2.get_name()+" wins the game");
-			player1.increase_chips(-p1Bet);
-			player2.increase_chips(p2Bet);
+			System.out.println(player2.getName()+" wins the game");
+			player1.increaseChips(-p1Bet);
+			player2.increaseChips(p2Bet);
 		}else{
 			System.out.println("Need another game");
 		}
-		System.out.println(player1.get_name()+" has "+player1.get_current_chips()+" chips");
-		System.out.println(player2.get_name()+" has "+player2.get_current_chips()+" chips");
+		System.out.println(player1.getName()+" has "+player1.getCurrentChips()+" chips");
+		System.out.println(player2.getName()+" has "+player2.getCurrentChips()+" chips");
 
 	}
 	
